7d1f77902e60285b9bf250983e5ad5a073f14ae7
iamnitindabas <nitindabas1000@gmail.com>
2024-04-04 11:51:45 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  Router,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import MainLayout from "./Layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
+
+// here index is path and then element
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      //:id represents that its a variable id not literally id
+      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
