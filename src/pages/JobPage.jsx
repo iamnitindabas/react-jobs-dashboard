edc97ce56c9fd8494a250a37dfd07af50dde3fa3
iamnitindabas <nitindabas1000@gmail.com>
2024-04-06 19:18:34 +0530

    project almost finshed

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,12 +1,23 @@
 // import { useState, useEffect } from "react";
-import { useParams, useLoaderData } from "react-router-dom";
+import { useParams, useLoaderData, Link, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
 // import Spinner from "../components/Spinner";
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
+  const navigate = useNavigate();
   const { id } = useParams(); //destructuring and getting id from parameters
   const job = useLoaderData();
+
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this listing?"
+    );
+    if (!confirm) return;
+    deleteJob(jobId);
+    toast.success("Job deleted successfully");
+    navigate("/jobs");
+  };
   //   const [job, setJob] = useState(null);
   //   const [loading, setLoading] = useState(true);
 
@@ -54,15 +65,10 @@ const JobPage = () => {
 
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-indigo-800 text-lg font-bold mb-6">
-                  {job.description}
+                  Job Description
                 </h3>
 
-                <p className="mb-4">
-                  We are seeking a talented Front-End Developer to join our team
-                  in Boston, MA. The ideal candidate will have strong skills in
-                  HTML, CSS, and JavaScript, with experience working with modern
-                  JavaScript frameworks such as React or Angular.
-                </p>
+                <p className="mb-4">{job.description}</p>
 
                 <h3 className="text-indigo-800 text-lg font-bold mb-2">
                   Salary
@@ -101,12 +107,15 @@ const JobPage = () => {
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  to={`/jobs/edit/${job.id}`}
+                  to={`/edit-job/${job.id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button
+                  onClick={() => onDeleteClick(job.id)}
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
                   Delete Job
                 </button>
               </div>
